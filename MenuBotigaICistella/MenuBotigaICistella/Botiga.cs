928c9ee241ab9950e9ef1fd078443422364e229f
nilpuerto <137113210+nilpuerto@users.noreply.github.com>
2024-03-07 11:26:15 +0100

    Començament Treball
    Falten classes i mètodes

---
+++ MenuBotigaICistella/MenuBotigaICistella/Botiga.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MenuBotigaICistella
+{
+    internal class Botiga
+    {
+        private string nomBotiga;
+        private int nElements;
+        private Producte[] productes;
+
+        public Botiga()
+        {
+            productes = new Producte[10];
+            nElements = 0;
+        }
+
+        public Botiga(string nomBotiga, int nombreProductes)
+        {
+            this.nomBotiga = nomBotiga;
+            productes = new Producte[nombreProductes];
+            nElements = 0;
+        }
+
+        public Botiga(string nomBotiga, Producte[] productes)
+        {
+            this.nomBotiga = nomBotiga;
+            this.productes = productes;
+            nElements = productes.Length;
+        }
+
+
+
+        public int EspaiLliure()
+        {
+            return productes.Length - nElements;
+        }
+    }
+}
